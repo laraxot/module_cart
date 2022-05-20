d2147a8ff7897b830348211cbc840e477f8a85d7
Malebestia <malebestia@yahoo.it>
2022-05-20 11:50:53 +0200

    .

--- Resources/views/booking/index/acts/add_booking.blade.php
+++ Resources/views/booking/index/acts/add_booking.blade.php
@@ -7,13 +7,11 @@
     <div class="flex flex-wrap pt-8">
         <div class="w-full md:w-1/3 mx-auto p-3">
 
-            @livewire('cart::calendar', [
+            @livewire('calendar', [
                 'minDate' => Carbon\Carbon::now()->subMonth(2)->toDateString(),
                 'maxDate' => Carbon\Carbon::now()->addMonth(2)->toDateString(),
             ])
-            {{--
-                @livewire('formx::numberer')
-            --}}
+            {{-- @livewire('formx::numberer') --}}
 
         </div>
     </div>
--- Resources/views/livewire/opening_hours.blade.php
+++ Resources/views/livewire/opening_hours.blade.php
@@ -1,80 +1,78 @@
 <div>
-    @component('theme::components.modal.simple',['guid'=>$modal_guid,'title'=>$modal_title])
-    @slot('content')
-        @if (session()->has('message'))
-            <div class="alert alert-success">
-                {{ session('message') }}
-            </div>
-        @endif
-        [{{ $status }}]
-        @foreach ($fields as $field)
-            @if ($field->view)
-                @include($field->view)
-            @else
-                {!! $field->html() !!}
+    @component('theme::components.modal.simple', ['guid' => $modal_guid, 'title' => $modal_title])
+        @slot('content')
+            @if (session()->has('message'))
+                <div class="alert alert-success">
+                    {{ session('message') }}
+                </div>
             @endif
-        @endforeach
-    @endslot
-    @slot('btns')
-
-        <button type="button" wire:click.prevent="cancel()" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        @if($status=='create')
-        <button class="btn btn-primary" wire:click.prevent="store()">{{ __('Create') }}</button>
-        @endif
-        @if($status=='edit')
-        <button class="btn btn-primary" wire:click.prevent="update()">{{ __('Update') }}</button>
-        @endif
-    @endslot
-    @endcomponent
-<div class="card border-0 shadow mb-5">
-	<div class="card-header bg-gray-100 py-4 border-0">
-		<div class="media align-items-center">
-			<div class="media-body">
-				<h4 class="mb-0">@lang('food::txt.Opening Hours')</h4>
-            </div>
-            <button type="button" class="btn btn-secondary mb-2" data-toggle="modal" data-target="#{{ $modal_guid }}" wire:click="create()">
-                <i class="fa fa-edit"></i>
-            </button>
-            {{--
-            <svg class="svg-icon svg-icon svg-icon-light w-3rem h-3rem ml-3 text-muted">
-				<use xlink:href="#wall-clock-1"> </use>
-            </svg>
-            --}}
-		</div>
-	</div>
-	<div class="card-body">
-		<table class="table text-sm">
-            @for($day=0; $day<=6; $day++)
-            <tr>
-                <th class="pl-0 border-0">
-                    @lang('food::txt.day_names_short.'.$week[$day])
-                </th>
-                @php
-                $orari=$row->openingHours->where('day_of_week',$day);
-                @endphp
-                @if(count($orari) == 0)
-                    <td class="pr-0 text-left border-0" style="padding-left: 0px;">
-                        ND
-                    </td>
+            [{{ $status }}]
+            @foreach ($fields as $field)
+                @if ($field->view)
+                    @include($field->view)
                 @else
-                    @foreach($orari as $v)
-                        <td class="pr-0 text-left border-0" style="padding-left: 0px;">
-                        <button wire:click="edit({{ $v->id }})" data-toggle="modal" data-target="#{{ $modal_guid }}">
-                        @if($v->is_closed)
-                            @lang('food::txt.closed')
-                        @else
-                            {{ substr($v->open_at,0,-3) }} - {{ substr($v->close_at,0,-3) }}
-                        @endif
-                        </button>
-                        {{-- Nested da degli errori
-                        @livewire('cart::opening_hours.item',['model'=>$v])
-                        --}}
-                        </td>
-                    @endforeach
+                    {!! $field->html() !!}
                 @endif
-            </tr>
-            @endfor
-		</table>
-	</div>
-</div>
+            @endforeach
+        @endslot
+        @slot('btns')
+            <button type="button" wire:click.prevent="cancel()" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            @if ($status == 'create')
+                <button class="btn btn-primary" wire:click.prevent="store()">{{ __('Create') }}</button>
+            @endif
+            @if ($status == 'edit')
+                <button class="btn btn-primary" wire:click.prevent="update()">{{ __('Update') }}</button>
+            @endif
+        @endslot
+    @endcomponent
+    <div class="card border-0 shadow mb-5">
+        <div class="card-header bg-gray-100 py-4 border-0">
+            <div class="media align-items-center">
+                <div class="media-body">
+                    <h4 class="mb-0">@lang('food::txt.Opening Hours')</h4>
+                </div>
+                <button type="button" class="btn btn-secondary mb-2" data-toggle="modal"
+                    data-target="#{{ $modal_guid }}" wire:click="create()">
+                    <i class="fa fa-edit"></i>
+                </button>
+                {{-- <svg class="svg-icon svg-icon svg-icon-light w-3rem h-3rem ml-3 text-muted">
+				<use xlink:href="#wall-clock-1"> </use>
+            </svg> --}}
+            </div>
+        </div>
+        <div class="card-body">
+            <table class="table text-sm">
+                @for ($day = 0; $day <= 6; $day++)
+                    <tr>
+                        <th class="pl-0 border-0">
+                            @lang('food::txt.day_names_short.' . $week[$day])
+                        </th>
+                        @php
+                            $orari = $row->openingHours->where('day_of_week', $day);
+                        @endphp
+                        @if (count($orari) == 0)
+                            <td class="pr-0 text-left border-0" style="padding-left: 0px;">
+                                ND
+                            </td>
+                        @else
+                            @foreach ($orari as $v)
+                                <td class="pr-0 text-left border-0" style="padding-left: 0px;">
+                                    <button wire:click="edit({{ $v->id }})" data-toggle="modal"
+                                        data-target="#{{ $modal_guid }}">
+                                        @if ($v->is_closed)
+                                            @lang('food::txt.closed')
+                                        @else
+                                            {{ substr($v->open_at, 0, -3) }} - {{ substr($v->close_at, 0, -3) }}
+                                        @endif
+                                    </button>
+                                    {{-- Nested da degli errori
+                        @livewire('opening_hours.item',['model'=>$v]) --}}
+                                </td>
+                            @endforeach
+                        @endif
+                    </tr>
+                @endfor
+            </table>
+        </div>
+    </div>
 </div>
